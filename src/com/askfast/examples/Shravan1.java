eea62d45125c2b80ccb8e35c9463945fd014e58f
shravanask <shravan@dealservices.com>
2013-05-02 10:42:46 +0200

    updated ASKType with QuestionType defined now in the sdk

--- src/com/askfast/examples/Shravan1.java
+++ src/com/askfast/examples/Shravan1.java
@@ -1,6 +1,4 @@
-
 package com.askfast.examples;
-
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,110 +14,101 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
+import com.askcs.dialog.sdk.model.QuestionType;
 import com.askfast.config.DialogSettings;
 import com.askfast.model.AskFast;
-import com.askfast.model.AskFast.AskType;
 
-
-@Path( "/shravan1" )
+@Path("/shravan1")
 public class Shravan1
 {
-    private ArrayList<String> cSampleAnswers = new ArrayList<String>(
-        Arrays.asList( "Yup", "Nope" ) );
-    private ArrayList<String> cSampleResponses = new ArrayList<String>(
-        Arrays.asList( "Thanks for making time!",
-            "We will miss you!",
-            "Something went wrong in this conversation.." ) );
+	private ArrayList<String> cSampleAnswers = new ArrayList<String>(Arrays.asList("Yup", "Nope"));
+	private ArrayList<String> cSampleResponses = new ArrayList<String>(
+																		Arrays.asList(	"Thanks for making time!",
+																						"We will miss you!",
+																						"Something went wrong in this conversation.."));
 
-    static final Logger log = Logger.getLogger( Shravan1.class.getName() );
+	static final Logger log = Logger.getLogger(Shravan1.class.getName());
 
-    @GET
-    @Produces( "text/plain" )
-    public Response firstQuestion(
-        @QueryParam( "preferred_medium" ) String preferred_medium,
-        @QueryParam( "responder" ) String responder )
-    {
-        AskFast askFast = new AskFast( getUrl() );
-        askFast.ask( "Are you coming to my bday party at Rotterdam?",
-            AskType.CLOSED,
-            null );
-        askFast.addAnswer( cSampleAnswers.get( 0 ), getUrl() + "/questions/10" );
-        askFast.addAnswer( cSampleAnswers.get( 1 ), getUrl() + "/questions/11" );
-        askFast.addAnswer( "Appointment", getUrl() + "/questions/12" );
-        return askFast.endDialog();
-    }
+	@GET
+	@Produces("text/plain")
+	public Response firstQuestion(@QueryParam("preferred_medium") String preferred_medium,
+			@QueryParam("responder") String responder)
+	{
+		AskFast askFast = new AskFast(getUrl());
+		askFast.ask("Are you coming to my bday party at Rotterdam?", QuestionType.CLOSED, null);
+		askFast.addAnswer(cSampleAnswers.get(0), getUrl() + "/questions/10");
+		askFast.addAnswer(cSampleAnswers.get(1), getUrl() + "/questions/11");
+		askFast.addAnswer("Appointment", getUrl() + "/questions/12");
+		return askFast.endDialog();
+	}
 
-    protected static String getUrl()
-    {
-        return DialogSettings.HOST + "/shravan1";
-    }
+	protected static String getUrl()
+	{
+		return DialogSettings.HOST + "/shravan1";
+	}
 
-    @Path( "/questions/{question_no}" )
-    @GET
-    @Produces( "text/plain" )
-    @Consumes( "*/*" )
-    public Response getQuestionText(
-        @PathParam( "question_no" ) String question_no,
-        @QueryParam( "preferred_medium" ) String prefered_mimeType )
-    {
-        String result = null;
-        if ( question_no.equals( "10" ) )
-        {
-            result = cSampleResponses.get( 0 );
-        }
-        else if ( question_no.equals( "11" ) )
-        {
-            result = cSampleResponses.get( 1 );
-        }
-        else if ( question_no.equals( "12" ) )
-        {
-            result = "Transferring you to Appointment agent";
-        }
-        else
-        {
-            result = cSampleResponses.get( 2 );
-        }
-        return Response.ok( result ).build();
-    }
+	@Path("/questions/{question_no}")
+	@GET
+	@Produces("text/plain")
+	@Consumes("*/*")
+	public Response getQuestionText(@PathParam("question_no") String question_no,
+			@QueryParam("preferred_medium") String prefered_mimeType)
+	{
+		String result = null;
+		if (question_no.equals("10"))
+		{
+			result = cSampleResponses.get(0);
+		}
+		else if (question_no.equals("11"))
+		{
+			result = cSampleResponses.get(1);
+		}
+		else if (question_no.equals("12"))
+		{
+			result = "Transferring you to Appointment agent";
+		}
+		else
+		{
+			result = cSampleResponses.get(2);
+		}
+		return Response.ok(result).build();
+	}
 
-    @Path( "/questions/{id}" )
-    @POST
-    @Produces( "application/json" )
-    @Consumes( "*/*" )
-    public Response answerQuestion( String answer_json,
-        @PathParam( "id" ) String answerId,
-        @QueryParam( "preferred_medium" ) String preferred_medium,
-        @QueryParam( "responder" ) String responder )
-    {
-        log.setLevel( Level.WARNING );
-        log.info( "answer entered is: " + answer_json );
+	@Path("/questions/{id}")
+	@POST
+	@Produces("application/json")
+	@Consumes("*/*")
+	public Response answerQuestion(String answer_json, @PathParam("id") String answerId,
+			@QueryParam("preferred_medium") String preferred_medium,
+			@QueryParam("responder") String responder)
+	{
+		log.setLevel(Level.WARNING);
+		log.info("answer entered is: " + answer_json);
 
-        AskFast askFast = new AskFast( getUrl() );
-        if ( answerId.equals( "1" ) )
-        {
-            askFast.ask( "Are you coming to my bday party at Rotterdam?",
-                AskType.CLOSED,
-                null );
-            askFast.addAnswer( "Yup", getUrl() + "/questions/10" );
-            askFast.addAnswer( "Nope", getUrl() + "/questions/11" );
-        }
-        else if ( answerId.equals( "10" ) )
-        {
-            return askFast.say( cSampleResponses.get( 0 ) );
-        }
-        else if ( answerId.equals( "11" ) )
-        {
-            return askFast.say( cSampleResponses.get( 1 ) );
-        }
-        else if ( answerId.equals( "12" ) )
-        {
-            return askFast.redirect( getUrl() + "/questions/" + answerId,
-                DialogSettings.HOST + "/questionanswer" );
-        }
-        else if ( answerId.equals( "3" ) )
-        {
-            return askFast.say( cSampleResponses.get( 2 ) );
-        }
-        return askFast.endDialog();
-    }
+		AskFast askFast = new AskFast(getUrl());
+		if (answerId.equals("1"))
+		{
+			askFast.ask("Are you coming to my bday party at Rotterdam?", QuestionType.CLOSED, null);
+			askFast.addAnswer("Yup", getUrl() + "/questions/10");
+			askFast.addAnswer("Nope", getUrl() + "/questions/11");
+		}
+		else if (answerId.equals("10"))
+		{
+			return askFast.say(cSampleResponses.get(0));
+		}
+		else if (answerId.equals("11"))
+		{
+			return askFast.say(cSampleResponses.get(1));
+		}
+		else if (answerId.equals("12"))
+		{
+			return askFast.redirect(getUrl() + "/questions/" + answerId, DialogSettings.HOST
+																			+ "/questionanswer");
+		}
+		else if (answerId.equals("3"))
+		{
+			return askFast.say(cSampleResponses.get(2));
+		}
+		return askFast.endDialog();
+	}
 }
--- src/com/askfast/model/AskFast.java
+++ src/com/askfast/model/AskFast.java
@@ -1,6 +1,4 @@
-
 package com.askfast.model;
-
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -14,11 +12,11 @@ import com.askcs.dialog.sdk.QuestionBuilder;
 import com.askcs.dialog.sdk.model.Answer;
 import com.askcs.dialog.sdk.model.AnswerPost;
 import com.askcs.dialog.sdk.model.Question;
+import com.askcs.dialog.sdk.model.QuestionType;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-
 
 /**
  * A stateless implementation of the dialog handler
@@ -28,169 +26,154 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
  */
 public class AskFast
 {
-    private Question cQuestion;
-    private String baseURL;
+	private Question cQuestion;
+	private String baseURL;
 
-    private String getBaseURL()
-    {
-        return baseURL;
-    }
+	private String getBaseURL()
+	{
+		return baseURL;
+	}
 
-    private void setBaseURL( String baseURL )
-    {
-        this.baseURL = baseURL;
-    }
+	private void setBaseURL(String baseURL)
+	{
+		this.baseURL = baseURL;
+	}
 
-    public enum AskType
-    {
-        OPEN( Question.QUESTION_TYPE_OPEN ),
-        CLOSED( Question.QUESTION_TYPE_CLOSED ),
-        COMMENT( Question.QUESTION_TYPE_COMMENT ),
-        REFERRAL( Question.QUESTION_TYPE_REFERRAL );
+	public AskFast(String url)
+	{
+		setBaseURL(url);
+		if (cQuestion == null)
+		{
+			cQuestion = new Question("1", "text://", null);
+		}
+	}
 
-        private String fieldName;
+	/**
+	 * creates a response based on the value. Also ends the dialog
+	 * 
+	 * @param value
+	 * @return
+	 */
+	public Response say(String value)
+	{
+		cQuestion = new Question(UUID.randomUUID().toString(), value, QuestionType.COMMENT);
+		return endDialog();
+	}
 
-        private AskType( String name )
-        {
-            this.fieldName = name;
-        }
+	/**
+	 * asks a question
+	 * 
+	 * @param askText
+	 * @param next
+	 * @return
+	 */
+	public Response ask(String askText, QuestionType askType, String next)
+	{
+		cQuestion.setQuestion_text(askText);
+		cQuestion.setType(askType);
+		if (next != null)
+		{
+			cQuestion.setAnswers(new ArrayList<Answer>(Arrays.asList(new Answer("", next))));
+		}
+		return Response.ok().build();
+	}
 
-        public String getFieldName()
-        {
-            return fieldName;
-        }
-    }
+	/**
+	 * adds an answer corresponding to a question asked
+	 * 
+	 * @param answerText
+	 * @param next
+	 * @return
+	 */
+	public Response addAnswer(String answerText, String next)
+	{
+		if (cQuestion == null || next == null)
+		{
+			return Response.status(Status.NOT_ACCEPTABLE).build();
+		}
+		ArrayList<Answer> tAllAnswers = cQuestion.getAnswers();
+		if (tAllAnswers == null)
+		{
+			tAllAnswers = new ArrayList<Answer>();
+		}
 
-    public AskFast( String url )
-    {
-        setBaseURL( url );
-        if ( cQuestion == null )
-        {
-            cQuestion = new Question( "1", "text://", null );
-        }
-    }
+		Answer answer = new Answer(answerText, next);
+		tAllAnswers.add(answer);
+		cQuestion.setAnswers(tAllAnswers);
 
-    /**
-     * creates a response based on the value. Also ends the dialog
-     * 
-     * @param value
-     * @return
-     */
-    public Response say( String value )
-    {
-        cQuestion = new Question( UUID.randomUUID().toString(), value,
-            AskType.COMMENT.getFieldName() );
-        return endDialog();
-    }
+		return Response.ok(tAllAnswers).build();
+	}
 
-    /**
-     * asks a question
-     * 
-     * @param askText
-     * @param next
-     * @return
-     */
-    public Response ask( String askText, AskType askType, String next )
-    {
-        cQuestion.setQuestion_text( askText );
-        cQuestion.setType( askType.getFieldName() );
-        if ( next != null )
-        {
-            cQuestion.setAnswers( new ArrayList<Answer>(
-                Arrays.asList( new Answer( "", next ) ) ) );
-        }
-        return Response.ok().build();
-    }
+	/**
+	 * redirect the control to a new agent
+	 * 
+	 * @param redirectText
+	 *            : can be the text directly or a HTTP based url which contains the text
+	 * @param next
+	 *            : the URL where the question for the redirection agent is available
+	 * @return
+	 */
+	public Response redirect(String redirectText, String next)
+	{
+		ObjectMapper om = new ObjectMapper();
+		ObjectNode node = om.createObjectNode();
+		node.put("type", QuestionType.REFERRAL.getName());
+		node.put("url", next);
+		if (redirectText.contains("http") || redirectText.contains("https"))
+		{
+			node.put("question_text", redirectText);
+		}
+		else
+		{
+			node.put("question_text", "text://" + redirectText);
+		}
+		return Response.ok(node.toString()).build();
+	}
 
-    /**
-     * adds an answer corresponding to a question asked
-     * 
-     * @param answerText
-     * @param next
-     * @return
-     */
-    public Response addAnswer( String answerText, String next )
-    {
-        if ( cQuestion == null || next == null )
-        {
-            return Response.status( Status.NOT_ACCEPTABLE ).build();
-        }
-        ArrayList<Answer> tAllAnswers = cQuestion.getAnswers();
-        if ( tAllAnswers == null )
-        {
-            tAllAnswers = new ArrayList<Answer>();
-        }
+	/**
+	 * ends a dialog
+	 * 
+	 * @return
+	 */
+	@SuppressWarnings("deprecation")
+	public Response endDialog()
+	{
+		String questionBuiltString = QuestionBuilder.build(cQuestion, getBaseURL(), null);
+		return Response.ok(questionBuiltString).build();
+	}
 
-        Answer answer = new Answer( answerText, next );
-        tAllAnswers.add( answer );
-        cQuestion.setAnswers( tAllAnswers );
+	public String getAnswerTextForOpenQuestion(String answer_json)
+	{
+		AnswerPost answer = parseJSON(answer_json, AnswerPost.class);
+		if (answer != null)
+		{
+			return answer.getAnswer_text();
+		}
+		else
+		{
+			return null;
+		}
+	}
 
-        return Response.ok( tAllAnswers ).build();
-    }
-
-    /**
-     * redirect the control to a new agent
-     * 
-     * @param redirectText
-     *            : can be the text directly or a HTTP based url which contains
-     *            the text
-     * @param next
-     *            : the URL where the question for the redirection agent is
-     *            available
-     * @return
-     */
-    public Response redirect( String redirectText, String next )
-    {
-        ObjectMapper om = new ObjectMapper();
-        ObjectNode node = om.createObjectNode();
-        node.put( "type", AskType.REFERRAL.getFieldName() );
-        node.put( "url", next );
-        if ( redirectText.contains( "http" ) || redirectText.contains( "https" ) )
-        {
-            node.put( "question_text", redirectText );
-        }
-        else
-        {
-            node.put( "question_text", "text://" + redirectText );
-        }
-        return Response.ok( node.toString() ).build();
-    }
-
-    /**
-     * ends a dialog
-     * 
-     * @return
-     */
-    @SuppressWarnings( "deprecation" )
-    public Response endDialog()
-    {
-        String questionBuiltString = QuestionBuilder.build( cQuestion,
-            getBaseURL(),
-            null );
-        return Response.ok( questionBuiltString ).build();
-    }
-
-    public String getAnswerTextForOpenQuestion( String answer_json )
-    {
-        AnswerPost answer;
-        try
-        {
-            answer = new ObjectMapper().readValue( answer_json,
-                AnswerPost.class );
-            return answer.getAnswer_text();
-        }
-        catch ( JsonParseException e )
-        {
-            e.printStackTrace();
-        }
-        catch ( JsonMappingException e )
-        {
-            e.printStackTrace();
-        }
-        catch ( IOException e )
-        {
-            e.printStackTrace();
-        }
-        return null;
-    }
+	public static <T> T parseJSON(String json_scenario, Class<T> DeserializeClass)
+	{
+		try
+		{
+			T createdScenario = new ObjectMapper().readValue(json_scenario, DeserializeClass);
+			return createdScenario;
+		}
+		catch (JsonParseException e)
+		{
+			e.printStackTrace();
+		}
+		catch (JsonMappingException e)
+		{
+			e.printStackTrace();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
